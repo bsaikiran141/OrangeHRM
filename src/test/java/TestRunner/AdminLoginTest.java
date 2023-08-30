2c74f236a16c9ac12c65231dd690cb141047ffc9
saikiranreddy <saikiranreddy@LAPTOP-8SGR38AT>
2023-08-30 07:48:14 +0530

    e

--- src/test/java/TestRunner/AdminLoginTest.java
+++ src/test/java/TestRunner/AdminLoginTest.java
@@ -1,13 +1,13 @@
 package TestRunner;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.openqa.selenium.remote.html5.AddLocationContext;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+//import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="featurefiles/adminlogin.feature",glue="stepdefinitions",dryRun=false,plugin= {"com.cucumber.listener.ExtentCucumberFormatter:reports/admintest.html"})
 public class AdminLoginTest extends AbstractTestNGCucumberTests
 {
